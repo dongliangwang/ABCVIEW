aa8c7dd2b6e26fe9c636e02eda0cc95287dfbc6e
wangdongliang <546297209@qq.com>
2019-07-03 14:37:58 +0800

    no message

---
+++ ABCVIEW/Classes/MediaSquareView/ww.h
@@ -0,0 +1,17 @@
+//
+//  ww.h
+//  new
+//
+//  Created by Allin372 on 2019/5/20.
+//  Copyright © 2019 Allin372. All rights reserved.
+//
+
+#import <Foundation/Foundation.h>
+
+NS_ASSUME_NONNULL_BEGIN
+
+@interface ww : NSObject
+
+@end
+
+NS_ASSUME_NONNULL_END
---
+++ ABCVIEW/Classes/MediaSquareView/ww.m
@@ -0,0 +1,13 @@
+//
+//  ww.m
+//  new
+//
+//  Created by Allin372 on 2019/5/20.
+//  Copyright © 2019 Allin372. All rights reserved.
+//
+
+#import "ww.h"
+
+@implementation ww
+
+@end

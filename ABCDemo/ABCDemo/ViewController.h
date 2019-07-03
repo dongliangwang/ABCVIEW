765e9c87a478d9429727552a49207e7ebabb8122
wangdongliang <546297209@qq.com>
2019-07-03 14:13:02 +0800

    no message

---
+++ ABCDemo/ABCDemo/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  ABCDemo
+//
+//  Created by Allin372 on 2019/7/3.
+//  Copyright © 2019 Allin372. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
---
+++ ABCDemo/ABCDemo/ViewController.m
@@ -0,0 +1,23 @@
+//
+//  ViewController.m
+//  ABCDemo
+//
+//  Created by Allin372 on 2019/7/3.
+//  Copyright © 2019 Allin372. All rights reserved.
+//
+
+#import "ViewController.h"
+
+@interface ViewController ()
+
+@end
+
+@implementation ViewController
+
+- (void)viewDidLoad {
+    [super viewDidLoad];
+    // Do any additional setup after loading the view.
+}
+
+
+@end
